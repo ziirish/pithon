a47349c9d518224b80bbf90e4459c450ebf27813
Benj <bsans@capensis.fr>
2013-01-31 10:26:52 +0100

    add: getpixel and setpixel functions in order to scale the image to fit the screen resolution

--- src/pithon.c
+++ src/pithon.c
@@ -19,6 +19,70 @@ void setpixel(SDL_Surface *screen, int x, int y, Uint8 r, Uint8 g, Uint8 b)
     *pixmem32 = colour;
 }
 
+Uint32 getpixel(SDL_Surface *surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32 *)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to set */
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch(bpp) {
+    case 1:
+        *p = pixel;
+        break;
+
+    case 2:
+        *(Uint16 *)p = pixel;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+            p[0] = (pixel >> 16) & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = pixel & 0xff;
+        } else {
+            p[0] = pixel & 0xff;
+            p[1] = (pixel >> 8) & 0xff;
+            p[2] = (pixel >> 16) & 0xff;
+        }
+        break;
+
+    case 4:
+        *(Uint32 *)p = pixel;
+        break;
+    }
+}
+
 SDL_Surface *ScaleSurface(SDL_Surface *Surface, Uint16 Width, Uint16 Height)
 {
     if(!Surface || !Width || !Height)
@@ -36,8 +100,8 @@ SDL_Surface *ScaleSurface(SDL_Surface *Surface, Uint16 Width, Uint16 Height)
         for(x = 0; x < Surface->w; x++)
             for(o_y = 0; o_y < _stretch_factor_y; ++o_y)
                 for(o_x = 0; o_x < _stretch_factor_x; ++o_x)
-                    DrawPixel(_ret, (int)(_stretch_factor_x * x) + o_x,
-                        (int)(_stretch_factor_y * y) + o_y, ReadPixel(Surface, x, y));
+                    putpixel(_ret, (int)(_stretch_factor_x * x) + o_x,
+                        (int)(_stretch_factor_y * y) + o_y, getpixel(Surface, x, y));
  
     return _ret;
 }
@@ -58,8 +122,8 @@ void DrawScreen(SDL_Surface* screen, int h)
     bg_pos.y = 0;
 
     bg = IMG_Load("logo.jpg");
-//    bg_resized = ScaleSurface (bg, infos->current_w, infos->current_h);
-    SDL_BlitSurface(bg, NULL, screen, &bg_pos);
+    bg_resized = ScaleSurface (bg, infos->current_w, infos->current_h);
+    SDL_BlitSurface(bg_resized, NULL, screen, &bg_pos);
 /*
     for(y = 0; y < screen->h; y++ ) 
     {
